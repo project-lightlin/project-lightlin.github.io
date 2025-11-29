aaef4660b2b33b1473f9db4d8b679571128024cb
teduzhu <leaffeather@foxmail.com>
2025-11-29 14:28:52 +0800

    1

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -32,23 +32,14 @@ templates_path = ['_templates']
 #html_theme = 'sphinx_rtd_theme'
 html_theme = 'pydata_sphinx_theme'
 
+html_static_path = ['_static']
+
 # 添加缓存控制meta标签
 html_meta = {
     'Cache-Control': 'no-cache, no-store, must-revalidate',
     'Pragma': 'no-cache',
     'Expires': '0'
 }
-
-# 添加版本号或时间戳到静态资源URL，实现缓存破坏
-# 使用当前时间作为构建标识符
-import time
-build_id = str(int(time.time()))
-
-# 自定义CSS和JS文件的URL参数
-def setup(app):
-    # 添加构建ID到静态文件URL
-    app.add_config_value('build_id', build_id, 'env')
-    # 这会使Sphinx在生成HTML时使用新的URL参数
 
 
 # -- Options for EPUB output
